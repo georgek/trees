981e105ff671457f23889ede12df8510a17721f5
George Kettleborough <g.kettleborough@member.fsf.org>
2014-06-02 21:26:20 +0100

    Simple error handling in CLI

--- cli.lisp
+++ cli.lisp
@@ -4,10 +4,17 @@
   (when (< (length argv) 2)
     (format t "Usage: ~A matrix_file~%" (first argv))
     (return-from main 1))
-  (let ((filename (second argv)))
-    (pp-tree-print
-     (peek (ultrametric-lasso4
-            (csv-to-cords filename
-                          :delimiter #\,
-                          :labelled t))))))
+  (handler-case
+      (let* ((filename (second argv))
+             (cords (csv-to-cords filename
+                                  :delimiter #\,
+                                  :labelled t))
+             (forest (ultrametric-lasso4 cords)))
+        (format t "Distances given: ~D~%" (length cords))
+        (pp-tree-print (peek forest) :stream t)
+        (format t "Distances used: ~D~%"
+                (length (lassoed-tree-used-cords (peek forest)))))
+    (condition (err)
+      (format *error-output* "~A: ~%  ~S~%"
+              (class-name (class-of err)) err))))
 
