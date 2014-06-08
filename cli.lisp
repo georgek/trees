2f0d6be2f3dc4bb20953946669e6eb25b10be140
George Kettleborough <g.kettleborough@member.fsf.org>
2014-06-08 21:11:05 +0100

    Add cli options

--- cli.lisp
+++ cli.lisp
@@ -1,20 +1,48 @@
 (in-package :gk-trees)
 
+(defvar *program-name* "lasso")
+
+(defun usage ()
+  (format *standard-output*
+          "Usage: ~A [-n | -p | -t | -T] matrix_file~%" *program-name*)
+  1)
+
 (defun main (argv)
-  (when (< (length argv) 2)
-    (format t "Usage: ~A matrix_file~%" (first argv))
-    (return-from main 1))
-  (handler-case
-      (let* ((filename (second argv))
-             (cords (csv-to-cords filename
-                                  :delimiter #\,
-                                  :labelled t))
-             (forest (ultrametric-lasso4 cords)))
-        (format t "Distances given: ~D~%" (length cords))
-        (pp-tree-print (peek forest) :stream t)
-        (format t "Distances used: ~D~%"
-                (length (lassoed-tree-used-cords (peek forest)))))
-    (condition (err)
-      (format *error-output* "~A: ~%  ~S~%"
-              (class-name (class-of err)) err))))
+  (let ((*program-name* (first argv))
+        (argv (rest argv)))
+   (unless (consp argv)
+     (return-from main (usage)))
+   (handler-case
+       (let* ((output-mode :newick)
+              (filename nil)
+              cords forest)
+         (loop for arg in argv do
+              (if (char= (aref arg 0) #\-)
+                  (handler-case
+                      (setf output-mode
+                            (ecase (aref arg 1)
+                              (#\n :newick)
+                              (#\p :pretty)
+                              (#\t :tikz)
+                              (#\T :tikz-polar)))
+                    (type-error (err)
+                      (format *error-output* "Unrecognised argument: ~A~%"
+                              (type-error-datum err))))
+                  (setf filename arg)))
+         (when (null filename)
+           (return-from main (usage)))
+         (setf cords (csv-to-cords filename :delimiter #\, :labelled t))
+         (setf forest (ultrametric-lasso4 cords))
+         (case output-mode
+           (:pretty
+            (pp-tree-print (peek forest) :stream *standard-output*))
+           (:tikz
+            (tikz-tree-print (peek forest) *standard-output*))
+           (:tikz-polar
+            (tikz-tree-print-polar (peek forest) *standard-output*))
+           (otherwise
+            (tree-print (peek forest) *standard-output*))))
+     (condition (err)
+       (format *error-output* "~A: ~%  ~S~%"
+               (class-name (class-of err)) err)))))
 
